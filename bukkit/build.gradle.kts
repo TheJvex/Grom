b944a4f5edad0d7e462a50ccb315b64f5a675a0e
TheJvex <167934319+TheJvex@users.noreply.github.com>
2025-12-31 15:40:16 -0300

    Update build.gradle.kts

--- bukkit/build.gradle.kts
+++ bukkit/build.gradle.kts
@@ -57,8 +57,8 @@ dependencies {
 }
 
 bukkit {
-    name = "GrimAC"
-    author = "GrimAC"
+    name = "GromAC"
+    author = "GromAC"
     main = "ac.grim.grimac.platform.bukkit.GrimACBukkitLoaderPlugin"
     website = "https://grim.ac/"
     apiVersion = "1.13"

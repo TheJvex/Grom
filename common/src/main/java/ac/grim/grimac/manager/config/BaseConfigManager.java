ea070359918a93c69706900432720167646316fd
TheJvex <167934319+TheJvex@users.noreply.github.com>
2025-12-31 16:04:24 -0300

    Update BaseConfigManager.java

--- common/src/main/java/ac/grim/grimac/manager/config/BaseConfigManager.java
+++ common/src/main/java/ac/grim/grimac/manager/config/BaseConfigManager.java
@@ -21,7 +21,7 @@ public class BaseConfigManager {
     @Getter
     private boolean printAlertsToConsole = false;
     @Getter
-    private String prefix = "&bGrim &8»";
+    private String prefix = "&bGrom &8»";
     @Getter
     private String disconnectTimeout;
     @Getter

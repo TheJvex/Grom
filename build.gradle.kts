9086575c736bb80174fd02ba7b2f2fb38c15fe3f
TheJvex <167934319+TheJvex@users.noreply.github.com>
2026-01-02 12:18:16 -0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ import versioning.VersionUtil
 
 BuildConfig.init(project)
 
-val baseVersion = "1.0.0"
+val baseVersion = "1.0.1"
 group = "ac.grim.grimac"
 version = VersionUtil.computeVersion(baseVersion)
 description = "Libre simulation and Heuristic anticheat designed for 1.21 with 1.8–1.21 support, powered by PacketEvents 2.0."
